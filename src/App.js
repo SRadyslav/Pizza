52c41ba969f365ed2939bb672c85c00914134199
Radyslav_Shovkivskyi <r.shovkivskyi@gmail.com>
2022-10-14 11:15:36 +0300

    Fixed some warning and added event on button in pizzaBlock

--- src/App.js
+++ src/App.js
@@ -9,20 +9,20 @@ import PizzaBlock from './components/PizzaBlock.jsx'
 
 function App() {
   return (
-    <div class="wrapper">
+    <div className="wrapper">
       <Header />
-      <div class="content">
-        <div class="container">
-          <div class="content__top">
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
             <Categories />
             <Sort />
           </div>
-          <h2 class="content__title">Все пиццы</h2>
-          <div class="content__items">
-          <PizzaBlock title= "Mexico" price= "5" />
-          <PizzaBlock title= "Standard" price= "4.5" />
-          <PizzaBlock title= "4 cheeses" price= "5"/>
-          <PizzaBlock />
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title="Mexico" price="5" />
+            <PizzaBlock title="Standard" price="4.5" />
+            <PizzaBlock title="4 cheeses" price="5" />
+            <PizzaBlock />
           </div>
         </div>
       </div>
---
+++ toDo.md
@@ -0,0 +1,1 @@
+// merge css.modules with scss
